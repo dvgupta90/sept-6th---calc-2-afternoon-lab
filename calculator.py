ccc912565626ee90b26761a5bf233bebe678a4af
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-06 16:08:14 -0700

    all airthmetic funtions

--- calculator.py
+++ calculator.py
@@ -13,7 +13,21 @@ while True:
     token = input_string.split(" ")
     if token[0] == "q":
         break
-    else:
+    elif token [0] == "+":
         print (add(int(token[1]), int(token[2])))
+    elif token [0] == "-":
+        print (subtract(int(token[1]), int(token[2])))
+    elif token [0] == "*":
+        print (multiply(int(token[1]), int(token[2])))
+    elif token [0] == "/":
+        print (divide(int(token[1]), int(token[2])))
+    elif token [0] == "square":
+        print (square(int(token[1]))) 
+    elif token[0] == "cube":
+        print (cube(int(token[1]))) 
+    elif token [0] == "pow":
+        print (pow(int(token[1]), int(token[2])))
+    elif token [0] == "mod":
+        print (mod(int(token[1]), int(token[2])))              
 
         
